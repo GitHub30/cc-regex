d85107b246eb4464b87c4afc81b1f750ee04839f
user19 <eternalboost@gmail.com>
2022-08-15 14:58:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ if (String.prototype.match.toString().includes("[native code]")) {
     const 都道府県 = /(北海道|青森県|岩手県|宮城県|秋田県|山形県|福島県|茨城県|栃木県|群馬県|埼玉県|千葉県|東京都|神奈川県|新潟県|富山県|石川県|福井県|山梨県|長野県|岐阜県|静岡県|愛知県|三重県|滋賀県|京都府|大阪府|兵庫県|奈良県|和歌山県|鳥取県|島根県|岡山県|広島県|山口県|徳島県|香川県|愛媛県|高知県|福岡県|佐賀県|長崎県|熊本県|大分県|宮崎県|鹿児島県|沖縄県)/
     const ip = /((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}/
     const fqdn = /([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}/
+    const 電話番号 = /0[-0-9]{9,12}/
     const match = String.prototype.match
     String.prototype.match = function (regexp) {
         regexp = new RegExp(regexp.source.replaceAll(/\x{email}/.source, email.source), regexp.flags + email.flags)
@@ -11,6 +12,7 @@ if (String.prototype.match.toString().includes("[native code]")) {
         regexp = new RegExp(regexp.source.replaceAll(/\x{都道府県}/.source, 都道府県.source), regexp.flags + 都道府県.flags)
         regexp = new RegExp(regexp.source.replaceAll(/\x{ip}/.source, ip.source), regexp.flags + ip.flags)
         regexp = new RegExp(regexp.source.replaceAll(/\x{fqdn}/.source, fqdn.source), regexp.flags + fqdn.flags)
+        regexp = new RegExp(regexp.source.replaceAll(/\x{電話番号}/.source, 電話番号.source), regexp.flags + 電話番号.flags)
         return match.call(this, regexp)
     }
 }
